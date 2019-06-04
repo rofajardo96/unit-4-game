a431148f70f2fbbfa2f997b76d0d16f29902f714
Robbie F <el.robbie96@gmail.com>
2019-06-03 20:33:48 -0500

    added crystal images, rules, score container, and random number container

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,18 @@ var prevNum = 0;
 var start_reset = function () {
 
     $(".crystals").empty();
+
+    var images = [
+        'https://static.turbosquid.com/Preview/001208/294/QN/red-crystals-3D-model_D.jpg',
+        'http://cdn.shopify.com/s/files/1/1342/5013/products/giant_crystal_blue2_grande.jpg?v=1472097716',
+        'https://www.zerotoys.com/v/vspfiles/photos/MAGICALCRYSTALS-3.jpg',
+        'http://www.sciencetoystore.com/contents/media/l_crystal_yellow_.jpg',
+
+    ];
     
     random_res = Math.floor(Math.random() * 101) + 19;
 
-    $("#result").html('Random Result: ' + random_res);
+    $("#result").html('Random Number: ' + random_res);
     
     for(var i = 0; i < 4; i++){
         
@@ -19,10 +27,17 @@ var start_reset = function () {
             crystal.attr({
                 "class": 'crystal', "randomize-number": random
             });
+            crystal.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover"
+
+            });
 
         $(".crystals").append(crystal);
 
     }
+
+    $("#previous-number").html("Total Score:" + prevNum)
 }
 
 start_reset();
@@ -32,13 +47,15 @@ $(document).on('click', ".crystal", function () {
     var num = parseInt($(this).attr('randomize-number'));
     prevNum += num;
     
+    $("#previous-number").html("Total Score:" + prevNum);
+    
     console.log(prevNum);
     
     if(prevNum > random_res){
         
         losses++;
         
-        $("#losses").html(losses);
+        $("#losses").html("Losses:" + losses);
 
         prevNum = 0;
 
@@ -48,7 +65,7 @@ $(document).on('click', ".crystal", function () {
         
         wins++;
         
-        $("#wins").html(wins);
+        $("#wins").html("Wins:" + wins);
 
         prevNum = 0;
 
